7ec804d223513e277d10d33e47a073793ed66ebd
magnuskoch <magnus96@live.de>
2023-04-16 23:56:10 +0200

    Update

---
+++ src/app/components/admin/AdminDashboard.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import {FC, use, useEffect, useState} from 'react'
+import Input from '../general/inputs/input';
+import Link from 'next/link';
+import Button from '../glassmorph/Button';
+import LegalFormService from '../../services/legalForm'
+import InputWithLable from '../general/inputs/InputWithLable';
+import LegalFormList from '../legalForm/LegalFormList';
+import CheckBoxWithLabel from '../general/checkboxes/CheckboxWithLabel';
+import ClientOnly from '../general/ClientOnly';
+
+interface AdminDashboardProps{
+  
+}
+
+const AdminDashboard:FC<AdminDashboardProps> = ({
+})=>
+{
+    
+   
+       
+
+    return(
+      
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default AdminDashboard;
